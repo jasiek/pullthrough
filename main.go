0caf83853519600875ed7acfc72cc3bc5b1652e5
Jan Szumiec <jan.szumiec@gmail.com>
2016-09-01 15:16:08 +0100

    content gets saved in disk cache

--- main.go
+++ main.go
@@ -1,20 +1,76 @@
 package main
 
 import (
+	"github.com/pkg/errors"
 	"fmt"
 	"net/http"
 	"os"
 	"log"
 	"time"
+	"io"
+	"net/url"
+	"math/rand"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	file, err := os.Open("main.go")
-	fmt.Printf("%s", r.URL)
+var url_to_cached_filename = make(map[string]string)
+
+func RandomString(strlen int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	const chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+	result := make([]byte, strlen)
+	for i := 0; i < strlen; i++ {
+		result[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(result)
+}
+
+func save(body io.ReadCloser, filename string) {
+	f, err := os.Create("/tmp/" + filename)
 	if err != nil {
 		log.Fatal(err)
+	}
+	n, err := io.Copy(f, body)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "couldn't write to file"))
 	} else {
-		http.ServeContent(w, r, "lol.txt", time.Now(), file)
+		fmt.Println("written: %d\n", n)
+	}
+	defer body.Close()
+	defer f.Close()
+}
+
+func fetchAndSave(url *url.URL, filename string) {
+	uri := url.String()
+	fmt.Println(uri)
+	resp, err := http.Get(uri)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	save(resp.Body, filename)
+	url_to_cached_filename[uri] = filename	
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	uri := r.URL.String()
+	log.Println("GET " + uri)
+	filename, ok := url_to_cached_filename[uri]
+	if ok {
+		log.Println("content is in cache")
+		// fall through
+	} else {
+		filename := RandomString(32)
+		log.Println("content not in cache, creating new entry: " + filename)
+		fetchAndSave(r.URL, filename)
+		log.Println("content saved")
+	}
+	
+	file, err := os.Open("/tmp/" + filename)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "couldn't open file"))
+	} else {
+		log.Println("serving content")
+		http.ServeContent(w, r, "", time.Now(), file)
 		return
 	}
 }
